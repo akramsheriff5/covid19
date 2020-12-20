4cbaf7fa5c376bcf3ea4b074c8f7d34f1b44e119
akramsheriff05 <akramsheriff05042000@gmail.com>
2020-12-20 19:12:32 +0530

    deploy commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Customers from './countries_data'
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
